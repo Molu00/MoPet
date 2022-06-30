f2bc9434b109d934423a1610f2a84e4393804006
LAPTOP-F23OT9C4\user <cindy070814@gmail.com>
2022-06-30 23:26:16 +0800

    employee & member login

--- src/main/java/tw/com/MoPet/model/employeeRepository.java
+++ src/main/java/tw/com/MoPet/model/employeeRepository.java
@@ -1,9 +1,16 @@
 package tw.com.MoPet.model;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface employeeRepository extends JpaRepository<employee, Integer> {
+	
+	@Query("from employee where empEmail=:empEmail and empPwd=:empPwd")
+	public employee findByNameAndPwd(String empEmail, String empPwd);
+	
+	@Query("from employee where empEmail=:email")
+	public employee findByAccount(String email);
 
 }
